452d3c6b07ced3d1b62c6846382a3d6c3831b679
megalomania111 <megalomania281@gmail.com>
2017-06-24 09:27:40 +0300

    regi/entrancel

--- js/common.js
+++ js/common.js
@@ -412,3 +412,22 @@ jQuery(document).ready(function(){
 	});
 	}
 
+
+
+// плавный скролл. сингл
+jQuery(document).ready(function(){
+	jQuery(".categories_block").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = jQuery(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = jQuery(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		jQuery('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
